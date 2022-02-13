bf66f305c09f8aa3673c483546d1f5c613f650e3
João Azevedo <joaogfazevedo@gmail.com>
2022-02-12 22:31:37 -0300

    feat: Fim da aula 32

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,38 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import Product from "../typeorm/entities/Product";
+import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+
+interface IRequest {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+class UpdateProductService {
+public async execute({ id, name, price, quantity }: IRequest): Promise<Product>{
+        const productsRepository = getCustomRepository(ProductsRepository);
+        const product =  await productsRepository.findOne(id);
+        const productExists = await productsRepository.findByName(name);
+
+        if(!product){
+            throw new AppError('Product not found');
+        }
+
+        if(productExists && name != product.name) {
+            throw new AppError('There is already one product with this name');
+        }
+
+        product.name = name;
+        product.price = price;
+        product.quantity = quantity;
+
+        await productsRepository.save(product);
+
+        return product;
+    }
+
+}
+
+export default UpdateProductService;
