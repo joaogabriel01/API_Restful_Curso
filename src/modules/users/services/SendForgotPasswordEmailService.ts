b55410005aacca1f9f0c896869c6ca97cf31b634
João Azevedo <joaogfazevedo@gmail.com>
2022-02-24 23:44:20 -0300

    fix: Importação do repositório

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,7 @@
 import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
-import UserTokensRepository from "../typeorm/repositories/UserTokensRepository copy";
+import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
 
 interface IRequest {
     email: string;
