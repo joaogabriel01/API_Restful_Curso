6f9dcab50355818acbef015b8bef19d8c90ea529
João Azevedo <joaogfazevedo@gmail.com>
2022-02-28 17:05:36 -0300

    feat: Entidade de clientes

---
+++ src/modules/customers/typeorm/entities/Customer.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn, Entity } from "typeorm";
+
+@Entity('customers')
+class Customers {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default Customers;
