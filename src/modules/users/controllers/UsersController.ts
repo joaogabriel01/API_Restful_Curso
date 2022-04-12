802cef1042b8ae507767f1084db5231d38e09838
João Azevedo <joaogfazevedo@gmail.com>
2022-04-12 14:04:53 -0300

    feat: Lib Class Transformer

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import CreateUserService from "../services/CreateUserService";
 import ListUserService from "../services/ListUserService";
+import { instanceToInstance } from 'class-transformer';
+
 
 export default class UsersController {
     public async index(request:Request, response: Response): Promise<Response> {
@@ -10,7 +12,7 @@ export default class UsersController {
 
         const users = await listUser.execute();
 
-        return response.json(users);
+        return response.json(instanceToInstance(users));
     }
 
     public async create(request: Request, response: Response): Promise<Response> {
